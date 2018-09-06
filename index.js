29c1524075007ef8bc4eb095e485f9735704b210
jihaddeveloper <jihad.developer@gmail.com>
2018-09-06 14:21:49 +0600

    1. Added  subjects bulk data to index.js file and edited the routing method. 2.Added new lines of Subjects to students.ejs file.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 // Added Templating Engine EJS
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //Link of the server
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log('Our server is live on http://127.0.0.1:3000/');
 })
 
@@ -13,30 +13,43 @@ app.listen(3000,function(){
 //PUT
 //DELETE
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.get('/about', function(req, res){
+app.get('/about', function (req, res) {
     res.sendFile(__dirname + '/about.html');
 })
 
-app.get('/api', function(req, res){
+app.get('/api', function (req, res) {
     res.sendFile(__dirname + '/api.html');
 })
 
-app.get('/contact', function(req, res){
+app.get('/contact', function (req, res) {
     res.sendFile(__dirname + '/contact.html');
 })
 
 //Bulk Data
-var students={
-    1:'Mohammad',
-    2:'Jihad',
-    3:'Hossain'
+var students = {
+    1: {
+        name: 'Mohammad',
+        subjects: ['C', 'C++', 'Java', 'Mysql']
+    },
+    2: {
+        name: 'Jihad',
+        subjects: ['C#', 'React', '.Net', 'MSsql']
+    },
+    3: {
+        name: 'Hossain',
+        subjects: ['NodeJS', 'EJS', 'Firebase','MongoDB']
+    }
 }
 
-app.get('/students/:id', function(req, res){
+app.get('/students/:id', function (req, res) {
     //Rendering Template called students
-    res.render('students',{id : req.params.id,name : students[req.params.id]});
+    res.render('students', {
+        id : req.params.id,
+        name : students[req.params.id].name,
+        subjects : students[req.params.id].subjects
+    });
 })
