0637f9af694b6691d3cfe6f82e7f28d6ea447b79
jihaddeveloper <jihad.developer@gmail.com>
2018-09-09 18:34:36 +0600

    Added CRUD functions.

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,8 +1,11 @@
 const exppress = require('express');
 var router = exppress.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
 
+//Import Employee Model
 var {Employee} = require('../models/employee');
 
+//To get all the Employees
 router.get('/',(req, res)=>{
     Employee.find((err, docs)=>{
         if(!err){
@@ -10,6 +13,75 @@ router.get('/',(req, res)=>{
         }
         else{
             console.log('Error in Retriving Employees :'+ JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+//To get An Employee with ID
+router.get('/:id', (req, res)=>{
+    if(!ObjectId.isValid(req.params.id)){
+        return res.status(400).send('No record found with given id : ' + `${req.params.id}`);
+    }
+    Employee.findById(req.params.id, (err, docs)=>{
+        if(!err){
+            res.send(docs);
+        }
+        else{
+            console.log('Error in Retriving Employees :'+ JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+//To add An Employee's data
+router.post('/',(req, res)=>{
+    var emp = new Employee({
+        name : req.body.name,
+        position : req.body.position,
+        office : req.body.office,
+        salary : req.body.salary
+    });
+    emp.save((err, docs)=>{
+        if(!err){
+            res.send(docs);
+        }
+        else{
+            console.log('Error in Employee Addition :'+ JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+//Update Employee with ID
+router.put('/:id', (req, res)=>{
+    if(!ObjectId.isValid(req.params.id)){
+        return res.status(400).send('No record found with given id : ' + `${req.params.id}`);
+    }
+    var emp = {
+        name : req.body.name,
+        position : req.body.position,
+        office : req.body.office,
+        salary : req.body.salary
+    };
+    Employee.findByIdAndUpdate(req.params.id, {$set : emp}, {new : true},(err, doc)=>{
+        if(!err){
+            res.send(doc);
+        }
+        else{
+            console.log('Error in Employee Update :'+ JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+//Delete An Employee with ID
+router.delete('/:id', (req, res)=>{
+    if(!ObjectId.isValid(req.params.id)){
+        return res.status(400).send('No record found with given id : ' + `${req.params.id}`);
+    }
+    Employee.findByIdAndRemove(req.params.id, {$set : emp}, {new : true},(err, doc)=>{
+        if(!err){
+            res.send(doc);
+        }
+        else{
+            console.log('Error in Employee Delete :'+ JSON.stringify(err, undefined, 2));
         }
     });
 });
--- models/employee.js
+++ models/employee.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 var Employee = mongoose.model('Employee', {
-    id : {type : String},
     name : {type : String},
     position : {type : String},
     office : {type : String},
